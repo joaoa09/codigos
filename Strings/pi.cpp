fa5b44346594d8e92f2c1c07de7e910865c04a1f
joaoa09 <joaoassis0912@gmail.com>
2026-07-14 22:29:53 -0300

    strings: melhorias z e add pi

--- Strings/pi.cpp
+++ Strings/pi.cpp
@@ -1,10 +1,12 @@
-// Prefix Function
+// Prefix Function e KMP
 //
 // pi[i] = tamanho da maior borda de s[0..i]
 // borda é um prefixo que também é um sufixo da string
+// KMP armazena os matches da string com o padrão
 //
-//Complexidade:
-// O(n)
+// Complexidades:
+// pi: O(n)
+// KMP: O(n+m)
 
 vector<int> prefix_function(string s) {
     int n = s.size();
@@ -25,4 +27,28 @@ vector<int> prefix_function(string s) {
     }
 
     return pi;
+}
+
+vector<int> kmp_match(string pat, string text) {
+    vector<int> pi = prefix_function(pat);
+    vector<int> occ;
+
+    int j = 0;
+
+    for (int i = 0; i < (int)text.size(); i++) {
+        while (j > 0 && text[i] != pat[j]) {
+            j = pi[j - 1];
+        }
+
+        if (text[i] == pat[j]) {
+            j++;
+        }
+
+        if (j == (int)pat.size()) {
+            occ.push_back(i - j + 1);
+            j = pi[j - 1];
+        }
+    }
+
+    return occ;
 }
